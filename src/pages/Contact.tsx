2764aaf3f6dbcfbac94bdeac49d08d7fcc0fdd67
Zahin Syahiran Bin Zuhaidi <muhammadzahien@gmail.com>
2026-08-01 19:15:42 +0800

    style: Set contact section heading font size to 48px on desktop

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -17,7 +17,7 @@ const Contact = () => {
             {/* Content Column */}
             <div className="space-y-6">
               <div className="space-y-4">
-                <h1 className="font-display sm:text-4xl font-medium mb-4 sm:mb-6 tracking-tight leading-tight sm:leading-tight lg:leading-tight lg:text-4xl text-3xl">Are you looking for designer? Let's have a coffee chat.</h1>
+                <h1 className="font-display text-3xl sm:text-4xl lg:text-[48px] font-medium mb-4 sm:mb-6 tracking-tight leading-tight sm:leading-tight lg:leading-[56px]">Are you looking for designer? Let's have a coffee chat.</h1>
                 <p style={{
                 color: 'var(--body-text)'
               }} className="font-mono text-lg max-w-2xl leading-[27px] sm:leading-relaxed px-1 mb-6 sm:mb-8 sm:px-0 text-zinc-400 sm:text-sm">SEND ME A MESSAGE ON LINKEDIN OR AN EMAIL.</p>
