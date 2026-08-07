a14960aa1cdb056f4880e2b125f6ce2133149222
Zahin Syahiran Bin Zuhaidi <muhammadzahien@gmail.com>
2026-08-08 01:58:41 +0800

    style: remove croissant pastry holder and pastry box mockup from Flaaah gallery

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -100,8 +100,6 @@ const verdantSolarGridImages = ["/lovable-uploads/75b4d467-d07b-4a22-8523-6a9a55
 const flaaahImages = [
   "/Flaaah/flaaah-outlet-2.png",
   "/Flaaah/flaaah-outlet-signange.png",
-  "/Flaaah/croissant-pastry-holder.png",
-  "/Flaaah/pastry-box-mockup.png",
   "/Flaaah/pastry-takeout-box-isometric-cream.png",
   "/Flaaah/aa.png",
   "/Flaaah/ss.png",
